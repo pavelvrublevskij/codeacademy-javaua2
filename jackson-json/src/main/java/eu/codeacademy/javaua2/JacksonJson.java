6507e0d11175bb746bf72fa91f9811d0c294138a
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-03-20 20:20:12 +0200

    add jackson (json) dependency and use ObjectMapper for Serialization and Deserialization

--- jackson-json/src/main/java/eu/codeacademy/javaua2/JacksonJson.java
+++ jackson-json/src/main/java/eu/codeacademy/javaua2/JacksonJson.java
@@ -1,13 +1,20 @@
 package eu.codeacademy.javaua2;
 
-/**
- * Hello world!
- *
- */
-public class JacksonJson
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.codeacademy.javaua2.model.Person;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JacksonJson {
+    public static void main(String[] args) throws IOException {
+        Person person = new Person("Petras", 25);
+
+        File personJsonFile = new File("target/person.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(personJsonFile, person);
+
+        Person personDeserialized = objectMapper.readValue(personJsonFile, Person.class);
+        System.out.println(personDeserialized);
     }
 }
