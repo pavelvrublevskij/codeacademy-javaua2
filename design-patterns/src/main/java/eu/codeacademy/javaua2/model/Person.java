2acb66af627a680336a52bba60fdc0e57a3a0171
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-03-28 20:47:32 +0300

    design pattern (inner class) builder pattern (part 3), to have builder pattern using @Builder annotation lombok

--- design-patterns/src/main/java/eu/codeacademy/javaua2/model/Person.java
+++ design-patterns/src/main/java/eu/codeacademy/javaua2/model/Person.java
@@ -1,39 +1,11 @@
 package eu.codeacademy.javaua2.model;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class Person {
-
-    // variables
-    // getter, setters, constructor
-
     private final String name;
     private final String surname;
-
-    public Person(Builder builder) {
-        this.name = builder.name;
-        this.surname = builder.surname;
-    }
-
-    public static class Builder {
-        // builder code with constructor, variables and setters
-
-        private String name;
-        private String surname;
-
-        public Builder name(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder surname(String surname) {
-            this.surname = surname;
-            return this;
-        }
-
-        public Person build() {
-            return new Person(this);
-        }
-    }
 }
--- design-patterns/src/main/java/eu/codeacademy/javaua2/pattern/builder/BuilderPattern.java
+++ design-patterns/src/main/java/eu/codeacademy/javaua2/pattern/builder/BuilderPattern.java
@@ -5,7 +5,7 @@ import eu.codeacademy.javaua2.model.Person;
 public class BuilderPattern {
 
     public Person createPerson(String name, String surname) {
-        return new Person.Builder()
+        return Person.builder()
                 .name(name)
                 .surname(surname)
                 .build();
