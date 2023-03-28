a4766a47697ac4df222c77dbcab8d9e7456c5853
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-03-28 21:33:03 +0300

    inner annonim class using lembda

--- inner-class/src/main/java/eu/codeacademy/javaua2/innerclass/annonimus/AnnonimClassExample.java
+++ inner-class/src/main/java/eu/codeacademy/javaua2/innerclass/annonimus/AnnonimClassExample.java
@@ -2,19 +2,12 @@ package eu.codeacademy.javaua2.innerclass.annonimus;
 
 public class AnnonimClassExample {
 
-    private int doMathSum(int first, int second) {
-        class MathSum implements TwoDigits {
-            @Override
-            public int withTwoDigits(int first, int second) {
-                return first + second;
-            }
-        }
-
-        MathSum mathSum = new MathSum();
-        return mathSum.withTwoDigits(first, second);
+    public void init() {
+        this.doMath((first, second) -> first + second);
+        this.doMath((a, b) -> a - b);
     }
 
-    public void init() {
-        System.out.println(doMathSum(2, 5));
+    private void doMath(TwoDigits twoDigits) {
+        System.out.println(twoDigits.withTwoDigits(2, 5));
     }
 }
