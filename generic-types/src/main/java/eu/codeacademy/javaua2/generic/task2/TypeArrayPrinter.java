9c8244b77eb91f6910d0b4760b44e357452e5e99
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-04-03 21:28:57 +0300

    generic - tast 2 solution extended

--- generic-types/src/main/java/eu/codeacademy/javaua2/generic/task2/TypeArrayPrinter.java
+++ generic-types/src/main/java/eu/codeacademy/javaua2/generic/task2/TypeArrayPrinter.java
@@ -1,11 +1,13 @@
 package eu.codeacademy.javaua2.generic.task2;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TypeArrayPrinter<E> {
 
-    public void printArray(E[] elements) {
-        Arrays.stream(elements)
-                .forEach(System.out::println);
+    public List<E> printArray(E[] elements) {
+        return Arrays.stream(elements)
+                .collect(Collectors.toList());
     }
 }
--- generic-types/src/test/java/eu/codeacademy/javaua2/generic/task2/TypeArrayPrinterTest.java
+++ generic-types/src/test/java/eu/codeacademy/javaua2/generic/task2/TypeArrayPrinterTest.java
@@ -1,18 +1,26 @@
 package eu.codeacademy.javaua2.generic.task2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 class TypeArrayPrinterTest {
 
     @Test
     void printArrayIntegers() {
         Integer[] ints = new Integer[]{1, 2, 3};
-        new TypeArrayPrinter<Integer>().printArray(ints);
+        List<Integer> result = new TypeArrayPrinter<Integer>().printArray(ints);
+
+        Assertions.assertEquals(Arrays.toString(ints), Arrays.toString(result.toArray()));
     }
 
     @Test
     void printArrayStrings() {
         String[] strings = new String[]{"Code", "Academy", "World"};
-        new TypeArrayPrinter<String>().printArray(strings);
+        List<String> result = new TypeArrayPrinter<String>().printArray(strings);
+
+        Assertions.assertEquals(Arrays.toString(strings), Arrays.toString(result.toArray()));
     }
 }
