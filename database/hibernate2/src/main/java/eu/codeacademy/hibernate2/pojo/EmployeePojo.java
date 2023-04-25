eabf77042f23da667b1b5aaf18d52109d8fc9a2b
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-04-25 20:35:37 +0300

    database hibernate - add more @Column example with/without nullable, name, length

--- database/hibernate2/src/main/java/eu/codeacademy/hibernate2/pojo/EmployeePojo.java
+++ database/hibernate2/src/main/java/eu/codeacademy/hibernate2/pojo/EmployeePojo.java
@@ -1,5 +1,6 @@
 package eu.codeacademy.hibernate2.pojo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,15 @@ public class EmployeePojo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // IDENTITY because postgresql
     private Long id;
 
+    @Column(length = 30, nullable = false)
     private String name;
+
+    @Column(length = 50, nullable = false)
     private String surname;
+
+    @Column(length = 30)
+    private String email;
+
+    @Column(name="phone_number", length = 8)
+    private String phoneNumber;
 }
