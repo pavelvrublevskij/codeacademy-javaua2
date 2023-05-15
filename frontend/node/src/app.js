b554aca2cfd7f630b4c36ee3feddb20aa3883909
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-15 18:15:33 +0300

    (pask5) frontend - add node project template fix

---
+++ frontend/node/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.use("/css",	express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")))
+app.use("/js",	express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")))
+app.use("/js", express.static(path.join(__dirname, "../node_modules/jquery/dist")))
+app.use("/js", express.static(path.join(__dirname, "../dist")))
+app.use("/style", express.static(path.join(__dirname, "../public/css")))
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
+app.listen(5000, () => {
+	console.log("Listening on port " + 5000)
+})
