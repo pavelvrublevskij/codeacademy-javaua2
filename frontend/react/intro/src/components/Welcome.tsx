44bf1145ff0fddeebb7b1a355a5c6dc8a9165b7c
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-23 20:59:58 +0300

    [React] - add Welcome component

---
+++ frontend/react/intro/src/components/Welcome.tsx
@@ -0,0 +1,5 @@
+function Welcome() {
+	return <div>Hello, Coder!</div>
+}
+
+export default Welcome;
---
+++ frontend/react/intro/src/containers/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Welcome from "../components/Welcome";
+
+function App() {
+	const text = 'Coder';
+	return (
+		<div>
+			<Welcome />
+		</div>
+	);
+}
+
+export default App;
