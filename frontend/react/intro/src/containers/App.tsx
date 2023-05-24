a9260771c3871073046a02f2dbe98d890b855187
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-24 21:07:55 +0300

    [React] - add global css style for whole project

--- frontend/react/intro/src/containers/App.tsx
+++ frontend/react/intro/src/containers/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from "./Login";
+import Task2 from "../task/Task2";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			pagal salyga ar admin ar user surenderint atitinkama rezultata (panaudoti skirtingus komponentus)
 			<hr/>
 			<Login loginName={'admin'} item1={100} item2={200}/>
+			<Task2 />
 		</div>
 	);
 }
