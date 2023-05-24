9319905db1df41808eb6a866c30696c29b9fa400
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-24 21:51:01 +0300

    [React] - clean up

--- frontend/react/intro/src/containers/App.tsx
+++ frontend/react/intro/src/containers/App.tsx
@@ -4,14 +4,8 @@ import Task2 from "../task/Task2";
 
 function App() {
 	return (
-		<div>
-			Uzd 1:
-			<br/>
-			pagal salyga ar admin ar user surenderint atitinkama rezultata (panaudoti skirtingus komponentus)
-			<hr/>
-			<Login loginName={'admin'} item1={100} item2={200}/>
-			<Task2 />
-		</div>
+		<>
+		</>
 	);
 }
 
