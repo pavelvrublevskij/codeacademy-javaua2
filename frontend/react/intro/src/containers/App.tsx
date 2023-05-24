33d188e24e84d117c2315047600e630a3bdde146
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-24 19:44:59 +0300

    [React] - clean up

--- frontend/react/intro/src/containers/App.tsx
+++ frontend/react/intro/src/containers/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import Welcome from "../components/Welcome";
 
 function App() {
-	const welcome =
-		<Welcome user={{
-			name: "Petras",
-			surname: "Petraitis"
-		}} salary={1500}/>;
-
-	const loopOf5Welcome = () => {
-		return Array.from(Array(5).keys())
-			.map((val) => welcome);
-	}
 
 	return (
 		<div>
-			{loopOf5Welcome()}
 		</div>
 	);
 }
