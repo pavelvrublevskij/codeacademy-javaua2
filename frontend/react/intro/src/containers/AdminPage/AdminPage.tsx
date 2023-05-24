c1b0815152e30e02a139f4187d667a627e5e59a0
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-24 21:41:52 +0300

    [React] - add bootstrap and react-bootstrap example

--- frontend/react/intro/src/containers/AdminPage/AdminPage.tsx
+++ frontend/react/intro/src/containers/AdminPage/AdminPage.tsx
@@ -1,12 +1,19 @@
 import "./AdminPage.css";
+import MyAlert from "../../component/MyALert";
+import React from "react";
 
 const AdminPage = (props: any) => {
 	return (
-		<div className={"AdminPage-backgroundColor"}>
-			Prisijungete kaip Admin
-			<div>Prisijgimo login yra {props.loginName}</div>
-			<div>Kiti propsai: {props.item1}, {props.item2}</div>
-		</div>
+		<>
+			<MyAlert/>
+			<div className={"AdminPage-backgroundColor"}>
+				<div className={"d-flex justify-content-center"}>
+					Prisijungete kaip Admin
+					<div>Prisijgimo login yra {props.loginName}</div>
+					<div>Kiti propsai: {props.item1}, {props.item2}</div>
+				</div>
+			</div>
+		</>
 	)
 }
 
