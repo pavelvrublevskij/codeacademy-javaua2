ae94782a50e6eb00418d095195b3621093ffee62
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-24 19:18:02 +0300

    [React] -use component in declaration and loop in function for rendering

--- frontend/react/intro/src/containers/App.tsx
+++ frontend/react/intro/src/containers/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Welcome from "../components/Welcome";
 
 function App() {
+	const welcome =
+		<Welcome user={{
+			name: "Petras",
+			surname: "Petraitis"
+		}} salary={1500}/>;
+
+	const loopOf5Welcome = () => {
+		return Array.from(Array(5).keys())
+			.map((val) => welcome);
+	}
+
 	return (
 		<div>
-			<Welcome user={{
-				name: "Petras",
-				surname: "Petraitis"
-			}} salary={1500}/>
+			{loopOf5Welcome()}
 		</div>
 	);
 }
