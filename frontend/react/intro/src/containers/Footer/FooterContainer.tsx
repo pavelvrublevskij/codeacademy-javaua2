c52c2006ba89a4a95a2555334808f6c926974aa6
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-25 18:28:35 +0300

    [React] - implement Footer

--- frontend/react/intro/src/containers/Footer/FooterContainer.tsx
+++ frontend/react/intro/src/containers/Footer/FooterContainer.tsx
@@ -1,5 +1,13 @@
+import {Container} from "react-bootstrap";
+
 const FooterContainer = () => {
-	return <span>Footer</span>
+	return (
+		<footer>
+			<Container fluid={'sm'}>
+				<span className={'text-muted'}>CodeAcademy, Ract Intro. All Right reserved :)</span>
+			</Container>
+		</footer>
+	)
 }
 
 export default FooterContainer;
