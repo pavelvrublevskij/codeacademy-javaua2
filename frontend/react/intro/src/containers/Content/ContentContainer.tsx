eff16702c930c5b43db34a4545b0a97898ad4e85
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-25 20:16:39 +0300

    [React] - use simple html form with js function onSubmit

--- frontend/react/intro/src/containers/Content/ContentContainer.tsx
+++ frontend/react/intro/src/containers/Content/ContentContainer.tsx
@@ -1,5 +1,21 @@
 const ContentContainer = () => {
-	return <span>Content</span>
+	const onSubmitEvent = (event: any) => {
+		event.preventDefault();
+		console.log(event);
+		console.log(event.target[0].value);
+	}
+
+	return (
+		<>
+			<form id="myForm" onSubmit={onSubmitEvent}>
+				<label htmlFor="fname">First name:</label>
+				<input type="text" id="fname" name="fname"/><br/><br/>
+				<label htmlFor="lname">Last name:</label>
+				<input type="text" id="lname" name="lname"/><br/><br/>
+				<input type="submit" value="Submit"/>
+			</form>
+		</>
+	)
 }
 
 export default ContentContainer;
