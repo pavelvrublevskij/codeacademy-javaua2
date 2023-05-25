c9bef898b2cff3af122b82a9e0c6503f660e2965
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-25 21:39:19 +0300

    [React] - use hook

--- frontend/react/intro/src/containers/Content/ContentContainer.tsx
+++ frontend/react/intro/src/containers/Content/ContentContainer.tsx
@@ -1,21 +1,52 @@
+import {Button, Form} from 'react-bootstrap';
+import {useState} from "react";
+
 const ContentContainer = () => {
+
+	const [result, setResult] = useState("Test");
+
 	const onSubmitEvent = (event: any) => {
 		event.preventDefault();
-		console.log(event);
 		console.log(event.target[0].value);
+		console.log(event.target[1].value);
+		console.log(event.target[2].value);
+		console.log(event.target[3].value);
+
+
+		setResult(event.target[0].value);
 	}
 
 	return (
 		<>
-			<form id="myForm" onSubmit={onSubmitEvent}>
-				<label htmlFor="fname">First name:</label>
-				<input type="text" id="fname" name="fname"/><br/><br/>
-				<label htmlFor="lname">Last name:</label>
-				<input type="text" id="lname" name="lname"/><br/><br/>
-				<input type="submit" value="Submit"/>
-			</form>
+			<Form onSubmit={onSubmitEvent}>
+				<Form.Group className='mb-3'>
+					<Form.Label>Product name</Form.Label>
+					<Form.Control placeholder='Write name'/>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='quantity'>
+					<Form.Label>Product quantity</Form.Label>
+					<Form.Control placeholder='Write quantity'/>
+				</Form.Group>
+
+				<Form.Group className='mb-3'>
+					<Form.Label>Product price</Form.Label>
+					<Form.Control placeholder='Write price'/>
+				</Form.Group>
+
+				<Form.Group className='mb-3'>
+					<Form.Label>Product description</Form.Label>
+					<Form.Control placeholder='Write description'/>
+				</Form.Group>
+
+				<Button variant='primary' type='submit'>
+					Submit
+				</Button>
+			</Form>
+
+			<div>{result}</div>
 		</>
-	)
+	);
 }
 
 export default ContentContainer;
