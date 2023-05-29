6c3e08ebbda46d143fcbff9f892ce245cdea55f2
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-29 20:30:27 +0300

    [React] - react router dom install with router example

--- frontend/react/intro/src/containers/App.tsx
+++ frontend/react/intro/src/containers/App.tsx
@@ -3,14 +3,19 @@ import HeaderContainer from "./Header/HeaderContainer";
 import ContentContainer from "./Content/ContentContainer";
 import FooterContainer from "./Footer/FooterContainer";
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 	return (
-		<div className={'mainApp'}>
-			<HeaderContainer/>
-			<ContentContainer/>
-			<FooterContainer/>
-		</div>
+		<BrowserRouter>
+			<div className={'mainApp'}>
+				<HeaderContainer/>
+				<Routes>
+					<Route path={'/example'} element={ <ContentContainer /> } />
+				</Routes>
+				<FooterContainer/>
+			</div>
+		</BrowserRouter>
 	);
 }
 
