3205411971f0acd6fa9eb7163ceb4d039b76480b
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-29 20:34:51 +0300

    [React] - react router dom create home page

--- frontend/react/intro/src/containers/App.tsx
+++ frontend/react/intro/src/containers/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import HeaderContainer from "./Header/HeaderContainer";
-import ContentContainer from "./Content/ContentContainer";
+import ContentContainer from "./Content/examples/NewProductHooksExample/ContentContainer";
 import FooterContainer from "./Footer/FooterContainer";
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Content/Home/Home";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<div className={'mainApp'}>
 				<HeaderContainer/>
 				<Routes>
+					<Route path={'/'} element={ <Home />} />
 					<Route path={'/example'} element={ <ContentContainer /> } />
 				</Routes>
 				<FooterContainer/>
