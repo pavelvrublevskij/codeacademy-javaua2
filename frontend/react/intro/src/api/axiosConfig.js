ecd8ae4fc3ec3e87e05708188efbedd0e47f31bd
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-29 21:28:42 +0300

    [React] - add axios library (api) and configure it

---
+++ frontend/react/intro/src/api/axiosConfig.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const apiKey = 'fd007d624cdfb5178dbfd4de1d381ed3';
+
+const API = axios.create({
+	baseURL: 'https://api.openweathermap.org/data/2.5'
+});
+
+export default API;
