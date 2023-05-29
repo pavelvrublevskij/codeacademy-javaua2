45973c6debfa9f370f990361c5fafd96f8819f3a
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-05-29 21:07:49 +0300

    [React] - react router dom and SPA (Single Page Application)

--- frontend/react/intro/src/containers/Header/HeaderContainer.tsx
+++ frontend/react/intro/src/containers/Header/HeaderContainer.tsx
@@ -1,10 +1,11 @@
 import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import {NavLink} from "react-router-dom";
 
 const HeaderContainer = () => {
 	return (
 		<Navbar bg="light" expand="lg">
 			<Container fluid>
-				<Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+				<Navbar.Brand to="/" as={ NavLink }>Navbar scroll</Navbar.Brand>
 				<Navbar.Toggle aria-controls="navbarScroll" />
 				<Navbar.Collapse id="navbarScroll">
 					<Nav
@@ -12,9 +13,9 @@ const HeaderContainer = () => {
 						style={{ maxHeight: '100px' }}
 						navbarScroll
 					>
-						<Nav.Link href="/">Home</Nav.Link>
+						<Nav.Link to="/" as={ NavLink }>Home</Nav.Link>
 						<NavDropdown title="Examples" id="navbarScrollingDropdown">
-							<NavDropdown.Item href="/example">New Product with hooks</NavDropdown.Item>
+							<NavDropdown.Item to="/example" as={ NavLink }>New Product with hooks</NavDropdown.Item>
 						</NavDropdown>
 					</Nav>
 					<Form className="d-flex">
