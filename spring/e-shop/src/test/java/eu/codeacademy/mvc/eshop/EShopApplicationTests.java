a4a0a24c685eaad6f872d1cf50f2c47a26fe5abc
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-06-01 20:16:14 +0300

    (spring) - [mvc] init empty eshop application

---
+++ spring/e-shop/src/test/java/eu/codeacademy/mvc/eshop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.mvc.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
