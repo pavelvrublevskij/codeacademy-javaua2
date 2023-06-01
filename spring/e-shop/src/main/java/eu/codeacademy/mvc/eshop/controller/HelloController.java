618fb43fb44982ee46e24b13dd18e45109488f68
Pavel Vrublevskij <pavel.vrublevskij@asprogramuoju.lt>
2023-06-01 20:53:53 +0300

    (spring) - [mvc] add template and controller with GetMapping

---
+++ spring/e-shop/src/main/java/eu/codeacademy/mvc/eshop/controller/HelloController.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.mvc.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello";
+	}
+}
